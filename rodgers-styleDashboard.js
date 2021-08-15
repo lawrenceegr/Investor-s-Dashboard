0daed769f0b0625bb647858fa254ee06be87be4d
LawrenceEgr <78215814+LawrenceEgr@users.noreply.github.com>
2021-08-15 23:31:27 +0300

    Add files via upload

---
+++ rodgers-styleDashboard.js
@@ -0,0 +1,13 @@
+var dropbtn = document.getElementById("dropdownMenuButton");
+dropbtn.onclick = ducument.getElementById("contents").style = 'display';
+var ref = document.getElementById("referrals");
+var withdr = document.getElementById("withdrawals");
+var dep = document.getElementById("deposits");
+var pend = document.getElementById("pending");
+ref.onclick = document.getElementById("demo1").innerHTML = 'demo5';
+dep.onclick = document.getElementById("demo1").innerHTML = demo2;
+withdr.onclick = document.getElementById("demo1").innerHTML = demo3;
+pend.onclick = document.getElementById("demo1").di = demo4;
+var pp = document.querySelector('#referrals');
+console.log(pp.outerHTML);
+pp.outerHTML = document.getElementById("demo1").onclick = demo5;
